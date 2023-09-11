877330dee9a10872aa0b69efed5dff6d90336d05
John Brian <njugunajb96@gmail.com>
2023-09-11 19:26:53 +0300

    create category model

---
+++ WingtipToys/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace WingtipToys.Models
+{
+    public class Category
+    {
+    }
+}
